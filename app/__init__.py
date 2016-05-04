3c78777231ed3b163cb09ad917c31119eaa95235
mussaimo <okemwamoses@gmail.com>
2016-05-04 12:43:01 +0300

    init the db

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+# imported sqlalchemy
+from flas.ext.sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config.from_object('config')
+db=SQLAlchemy(app)
 
-from app import views
+
+from app import views, models
 
