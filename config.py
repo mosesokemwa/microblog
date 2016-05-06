fb2683251a3d9716af71775d60a5a65d8426e3c9
mussaimo <okemwamoses@gmail.com>
2016-05-06 22:05:05 +0300

    database and migration helper scripts

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ OPENID_PROVIDERS = [
 
 
 import os
-dasedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
