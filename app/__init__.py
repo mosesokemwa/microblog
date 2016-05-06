43461882ed9eda1ac12a00c30b37ad6df48fcf76
mussaimo <okemwamoses@gmail.com>
2016-05-06 21:53:08 +0300

    created db columns

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
 # imported sqlalchemy
-from flas.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object('config')
-db=SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 
 from app import views, models
-
