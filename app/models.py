2ab8d860083ff2825988ecc081b392d6be0af57f
mussaimo <okemwamoses@gmail.com>
2016-05-07 20:00:16 +0300

    post model fo database

--- app/models.py
+++ app/models.py
@@ -7,4 +7,14 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
 
     def __repr__(self):
-        return '<User %r>' %(self.nickname)
+        return '<User %r>' % self.nickname
+
+
+class Post(db.model):
+    id = db.Column(db.integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.Datetime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post %r>' % self.body
